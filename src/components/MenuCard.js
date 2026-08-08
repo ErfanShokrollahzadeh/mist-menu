25fc13b880b7dc11bfdbda3488146773e7449891
ErfanShokrollahzadeh <erfan@example.com>
2026-08-08 21:13:39 +0300

    feat: integrate menu item images with dynamic UI rendering and update script

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -3,13 +3,20 @@
 export default function MenuCard({ item }) {
   return (
     <div className="menu-card">
-      <div className="card-top">
-        <span className="card-name">{item.name}</span>
-        <span className="card-price">{item.price}</span>
-      </div>
-      {item.description && (
-        <p className="card-description">{item.description}</p>
+      {item.image && (
+        <div className="menu-card-img-wrapper">
+          <img src={item.image} alt={item.name} className="menu-card-img" />
+        </div>
       )}
+      <div className="menu-card-content">
+        <div className="card-top">
+          <span className="card-name">{item.name}</span>
+          <span className="card-price">{item.price}</span>
+        </div>
+        {item.description && (
+          <p className="card-description">{item.description}</p>
+        )}
+      </div>
     </div>
   );
 }
